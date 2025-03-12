110b69796bd026636ab0e5147acb85a3a98d3605
svtln007 <lana_pirogova@vk.com>
2025-03-12 22:14:04 +0300

    оредактировала информацию

--- app/page.js
+++ app/page.js
@@ -7,24 +7,24 @@ export default function Home() {
       <h1 className={styles.title}>Мое резюме</h1>
       <section className={styles.section}>
         <h2 className={styles.sectionTitle}>Контакты</h2>
-        <p className={styles.sectionText}><strong>Имя:</strong> Александрова Анна Алексеевна</p>
+        <p className={styles.sectionText}><strong>Имя:</strong> Пирогова Светлана Павловна</p>
         <p className={styles.sectionText}><strong>Возраст:</strong> 21 год</p>
-        <p className={styles.sectionText}><strong>Email:</strong> my@gmail.com</p>
+        <p className={styles.sectionText}><strong>Email:</strong> lana_pirogova@vk.com</p>
         <p className={styles.sectionText}>
           <strong>GitHub:</strong>{" "}
-          <a href="https://github.com/Anna1Alexandrova" className={styles.link}>github.com/Anna1Alexandrova</a>
+          <a href="https://github.com/svtln007" className={styles.link}>github.com/SvetlanaPirogova</a>
         </p>
       </section>
       <section className={styles.section}>
         <h2 className={styles.sectionTitle}>Опыт работы</h2>
         <p className={styles.sectionText}>
-          <strong>Системный аналитик</strong> (1 год)
+          <strong>Технический секретарь</strong> (1 год)
         </p>
         <ul className={styles.list}>
-          <li>Анализ и формализация требований заказчиков.</li>
-          <li>Разработка технической документации.</li>
-          <li>Участие в проектировании архитектуры систем.</li>
-          <li>Взаимодействие с командой разработки и тестирования.</li>
+          <li>Работа с 1С.</li>
+          <li>Оформление документов.</li>
+          <li>Зачисление абитуриентов в ФГБОУ ВО ПГТУ.</li>
+          <li>Помощь с выбором жизненного пути.</li>
         </ul>
       </section>
       <section className={styles.section}>
@@ -38,10 +38,9 @@ export default function Home() {
       <section className={styles.section}>
         <h2 className={styles.sectionTitle}>Навыки</h2>
         <ul className={styles.list}>
-          <li>Анализ и проектирование бизнес-процессов.</li>
           <li>Работа с требованиями (BPMN, Use Case, User Stories).</li>
           <li>Базовые знания SQL.</li>
-          <li>Опыт работы с Jira, Confluence.</li>
+          <li>Опыт работы с 1С.</li>
           <li>Навыки работы в команде.</li>
         </ul>
       </section>
